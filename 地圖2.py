258450a58e704c9894bc5c3eee821689e8b6ba3c
aiko77777 <a21471957@yahoo.com>
2024-03-21 22:57:23 +0800

    "a method for passing the address with google map for LineBot"

--- 地圖2.py
+++ 地圖2.py
@@ -1,23 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-# response = requests.get("https://www.google.com/maps/place?q="+"東興公園")
-# soup = BeautifulSoup(response.text, "html.parser")
-# location=soup.find_all("meta")
-# locatiom_set={"ini"}    #初始值建立set
-# for i in location:
-#     for unit in list(i["content"]):
-#        #print(unit)
-#         if "%" in unit:
-#            #print(i["content"])
-#            locatiom_set.add(i["content"])   #使用set去除重複
-# locatiom_set.remove("ini")
-# string_location=(str(locatiom_set)) #使用字串摘取出經緯度
-# latitude_position1=string_location.find("=")    #節錄:center=25.04642248%2C121.51334263&
-# latitude_position2=string_location.find("%")
-# print(string_location[latitude_position1+1:latitude_position2:])
-# #longitude_position1=string_location.find("c")
-# longitude_position2=string_location.find("&")
-# print(string_location[latitude_position2+3:longitude_position2:])   #latitude的終點+3設為longitude的起點
+
 
 def get_location(keywords):
     response = requests.get("https://www.google.com/maps/place?q="+keywords)
